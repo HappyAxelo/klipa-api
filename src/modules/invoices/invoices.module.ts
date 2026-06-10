3d70d72f69cf2e32ae833f84ff2d8e61cef267a9
HappyAxelo <167855843+HappyAxelo@users.noreply.github.com>
2026-06-10 11:55:52 +0200

    Update invoices.module.ts

--- src/modules/invoices/invoices.module.ts
+++ src/modules/invoices/invoices.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { InvoicesController } from './invoices.controller';
+import { InvoicesController, InvoicesPublicController } from './invoices.controller';
 import { InvoicesService } from './invoices.service';
 import { InvoiceNumberService } from './invoice-number.service';
 import { CustomersModule } from '../customers/customers.module';
-
 @Module({
   imports: [CustomersModule],
-  controllers: [InvoicesController],
+  controllers: [InvoicesController, InvoicesPublicController],
   providers: [InvoicesService, InvoiceNumberService],
 })
 export class InvoicesModule {}
